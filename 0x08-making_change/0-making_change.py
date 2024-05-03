af18d333846fb1dd6410f749d21a2356844affcc
Ahmed Olalekan <Olalekanahmed6@gmail.com>
2024-05-03 02:43:35 +0100

    add solution

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Make coin change
+"""
+
+
+def makeChange(coins, total):
+    """
+        function that make coin change
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
