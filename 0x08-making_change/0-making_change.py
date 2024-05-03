95dde086431099d34fa5d5fd6165b1d9fff9e162
Ahmed Olalekan <Olalekanahmed6@gmail.com>
2024-05-03 02:49:06 +0100

    add solution

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,19 +6,18 @@ Make coin change
 
 
 def makeChange(coins, total):
-    """
-        function that make coin change
+    """ function that make changes
     """
     if total <= 0:
         return 0
 
+    coins.sort(reverse=True)
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
     if dp[total] == float('inf'):
         return -1
